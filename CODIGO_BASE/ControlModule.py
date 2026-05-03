04feda00567ed667f5fbbb2aadf385ca1e898e50
100524718 <100524718@alumnos.uc3m.es>
2026-05-03 17:41:34 +0200

    hemos acabado la funcion control_loop, en la que el bucle simula todos los pasos, donde decidimos con nuestro codigo ya creado la acción óptima en cada instante, aplicando el comportamiento aleatorio del reactor mediante np.random.choice. Además, garantizamos  que la potencia respete los límites del rango y normalizamos los resultados

--- CODIGO_BASE/ControlModule.py
+++ CODIGO_BASE/ControlModule.py
@@ -75,18 +75,50 @@ class ControlModule:
         #Le pedimos que nos de la politica optima y la guardamos en esta variable
         politica_optima = r.policy
         #pedimos que nos de la politica optima del estado actual
+        #que sera aquella que nos de la maxima recompensa
         mejor_accion = politica_optima[estado_actual]
         return np.int32(mejor_accion)
 
     @staticmethod
-    def control_loop(demand: np.ndarray, 
+    def control_loop(demanda: np.ndarray,
                      probabilidades: np.ndarray,
                      numero_estados: np.int32,
                      numero_acciones: np.int32,
                      factor_descuento: np.float64) -> np.ndarray:
         """ Function that computes all the required iterations (control-loop) to satisfy the power demand """
-        ### TO BE COMPLETED BY THE STUDENTS ###
+        #creamos la matriz de transicion P
+        matriz_transicion = ControlModule.generate_P(probabilidades, numero_estados, numero_acciones)
 
-        ### DUMMY BEHAVIOUR TO PREVENT CRASHING (MUST BE DELETED AFTER THE FULL IMPLEMENTATION) ###
-        return np.zeros_like(a=demand, dtype=np.float64)
-        ### ###
+        #creamos el array de ceros donde guardaremos la potencia que entrega el reactor en cada instante.
+        respuesta = np.zeros_like(demanda, dtype=np.float64)
+
+        #inicializamos el estado inicial del reactor en 0 porque se acaba de encender y definimos los efectos reales de las acciones.
+        estado_actual = 0
+        #efectos: decrease [-2,-1,0], mantain [-1,0,1], increase [0,1,2].
+        efectos_acciones = [[-2, -1, 0], [-1, 0, 1], [0, 1, 2]]
+
+        #recorremos cada punto de la demanda
+        for t in range(len(demanda)):
+            demanda_actual_t = demanda[t]
+
+            #el MDP decide que acción es la mejor para el estado y demanda actual.
+            accion_optima = ControlModule.control_iteration(matriz_transicion, demanda_actual_t, estado_actual, numero_estados,numero_acciones,factor_descuento)
+
+            #el reactor ejecuta la acción, pero con un componente de azar en funcion de las probabiladesde acierto y fallo de la accion elegida
+            #con np.random.choice eligimos uno de los 3 efectos posibles según las probabilidades del reactor
+            probs_de_la_accion = probabilidades[accion_optima]
+            desplazamiento_real = np.random.choice(efectos_acciones[accion_optima], probs_de_la_accion)
+            #actualizamos el estado
+            estado_actual += desplazamiento_real
+
+            #comprobamos los límites para no salirnos del rango 0-99
+            if estado_actual < 0:
+                estado_actual = 0
+            elif estado_actual >= numero_estados:
+                estado_actual = numero_estados - 1
+
+            #guardamos la potencia normalizada
+            respuesta[t] = estado_actual / numero_estados
+
+        #devolvemos la serie completa de la respuesta del reactor
+        return respuesta
