e2b945ce0763904f68bb602156c954aa7d8b78b6
Eduardo Fernández-Paniagua <100522392@alumnos.uc3m.es>
2026-05-03 10:50:49 +0200

    Eduardo Fernández-Paniagua - 100522392 - Comentario de contexto para el método control_iteration, para quien lo continúe de mis compañeros.

--- CODIGO_BASE/ControlModule.py
+++ CODIGO_BASE/ControlModule.py
@@ -62,6 +62,9 @@ class ControlModule:
     @staticmethod
     def control_iteration(matriz_transicion: np.ndarray, demanda_actual: np.float64, estado_actual = np.int32, numero_estados: np.int32, numero_acciones: np.int32, factor_descuento: np.float64) -> np.int32:
         """ Function that computes one control-iteration """
+        #Una cosa importante para contextualizar: Hemos planteado que supuestamente mdptoolbox trabaja con recompensas y antes ya en generate_R
+        #transformamos de costes a recompensas, entonces en el enunciado nos piden minimizar costes y con este enfoque que
+        #hemos planteado será basicamente maximizar recompensas.
         #Creamos la matriz de recompensas para la iteración actual en base a la demanda actual
         matriz_recompensas = ControlModule.generate_R(demanda_actual = demanda_actual, numero_estados = numero_estados, numero_acciones = numero_acciones)
 
