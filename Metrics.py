b82ecb5bbf540c609197e02bf3c8820368c64b96
100524718 <100524718@alumnos.uc3m.es>
2026-05-08 18:32:26 +0200

    CAMBIO LAS SALIDAS

--- Metrics.py
+++ Metrics.py
@@ -3,19 +3,19 @@ import numpy as np
 
 def MAE(y_true: np.ndarray, y_pred: np.ndarray) -> np.float64:
     """ Implementation of the Mean Absolute Error (MAE) """
-    return np.mean(np.abs(y_true - y_pred))
+    return np.float64(np.mean(np.abs(y_true - y_pred)))
 
 def MSE(y_true: np.ndarray, y_pred: np.ndarray) -> np.float64:
     """ Implementation of the Mean Squared Error (MSE) """
-    return np.mean((y_true - y_pred)**2)
+    return np.float64(np.mean((y_true - y_pred)**2))
 
 def R2(y_true: np.ndarray, y_pred: np.ndarray) -> np.float64:
     """ Implementation of the R2 metric """
     ss_res = np.sum((y_true - y_pred)**2)
     ss_tot = np.sum((y_true - np.mean(y_true))**2)
-    return 1 - (ss_res/ss_tot)
+    return np.float64(1 - (ss_res/ss_tot))
 
 def Corr(y_true: np.ndarray, y_pred: np.ndarray) -> np.float64:
     """ Implementation of the Pearson's Correlation Coefficient """
     corr = np.corrcoef(y_true, y_pred)
-    return corr[0, 1]
+    return np.float64(corr[0, 1])
